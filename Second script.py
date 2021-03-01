823adab629c033f46cc1249a616944eb6af002f4
Juan Manuel <juanmanueldiazarbues@gmail.com>
2021-03-01 19:34:39 -0300

    Polished up the original scripts.

--- Second script.py
+++ Second script.py
@@ -2,10 +2,10 @@ import numpy as np
 from gekko import GEKKO
 import matplotlib.pyplot as plt
 
-m = GEKKO()    #Create GEKKO model
-y = m.Var(0) #Create GEKKO variable
-m.Equation(y.dt()==-y+1) #Create GEKKO equation
-m.time = np.linspace(0,50) #Time points
+m = GEKKO()                 #Create GEKKO model
+y = m.Var(0)                #Create GEKKO variable
+m.Equation(y.dt()==-y+1)    #Create GEKKO equation
+m.time = np.linspace(0,50)  #Time points
 
 #Solve ODE
 m.options.IMODE = 4
@@ -15,4 +15,11 @@ m.solve()
 plt.plot(m.time,y)
 plt.xlabel('time')
 plt.ylabel('y(t)')
+
+#Calculate the value of y(t) as t tends to infinity
+m.options.IMODE = 3
+m.solve(disp=False)
+print('Final Value: ' + str(y.value))
+
+#Show results
 plt.show()
